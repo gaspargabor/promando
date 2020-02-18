43df72737931302cfc22b824fd7a2d0f586cae47
Vivien Walter <walter.vivien.k@gmail.com>
2020-02-18 12:09:24 +0100

    building with data

--- static/js/dom.js
+++ static/js/dom.js
@@ -16,46 +16,53 @@ export let dom = {
         // it adds necessary event listeners also
 
         for(let board of boards){
-
+            console.log(board);
+            console.log(board.id);
             const createBoard = function(title){
             const boardTemplate = document.querySelector('#board-template');
             const clone = document.importNode(boardTemplate.content, true);
             clone.querySelector('.board-title').textContent = title;
+            clone.querySelector('.board').setAttribute('id', 'board' + board.id);
+            clone.querySelector('.board-columns').setAttribute('id', 'columns' + board.id);
             return clone;
             };
             const singleBoard = createBoard(board.title);
-            document.querySelector('#boards').appendChild(singleBoard);}
+            document.querySelector('#boards').appendChild(singleBoard);
+            dom.loadCards(board.id);
+                for (let i=0; i<4; i++) {
+                    const createColumn = function(title){
+                    const columnTemplate = document.querySelector('#column-template');
+                    const clone = document.importNode(columnTemplate.content, true);
+                    clone.querySelector('.board-column-title').textContent = title;
+                    clone.querySelector('.board-column').setAttribute('id', 'board-col' + i);
+                    return clone;
+                    };
+                    const singleCol = createColumn('Column title');
+                    document.querySelector('#columns' + board.id).appendChild(singleCol);}}
 
-        for (let i=0; i<6; i++) {
-
-            const createColumn = function(title){
-            const columnTemplate = document.querySelector('#column-template');
-            const clone = document.importNode(columnTemplate.content, true);
-            clone.querySelector('.board-column-title').textContent = title;
-            return clone;
-            };
-            const singleCol = createColumn('Column title');
-            document.querySelector('.board-columns').appendChild(singleCol);}
-            dom.showCards('hjelo');
         },
 
 
 
         loadCards: function (boardId) {
             // retrieves cards and makes showCards called
+            dataHandler.getCardsByBoardId(function(boards){
+            dom.showCards(cards);
+        });
         },
         showCards: function (cards) {
             // shows the cards of a board
             // it adds necessary event listeners also
-
+            for (let card of cards) {
+                console.log(card);
             const createCard = function(title){
             const cardTemplate = document.querySelector('#card-template');
             const clone = document.importNode(cardTemplate.content, true);
             clone.querySelector('.card-title').textContent = title;
             return clone;
             };
-            const singleCol = createCard('Card title');
-            document.querySelector('.board-column-content').appendChild(singleCol);
+            const singleCol = createCard(card.cards_title);
+            document.querySelector('.board-column-content').appendChild(singleCol);}
         },
         // here comes more features
 };
